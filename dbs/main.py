bdeffef0b1752776f1844f447869182e59ce9d0f
fahad ali <fahad.ali@emumba.com>
2023-11-20 19:04:58 +0500

    Add logging print statments

--- dbs/main.py
+++ dbs/main.py
@@ -99,8 +99,7 @@ def build_client():
             verify=True,
             )
     except exceptions.KeycloakAuthenticationError as err:
-        print("raising exception for building client")
-        print(config["ADMIN_CLIENT_SECRET_KEY"])
+        print("admin cli is confidential, using client secret to login")
         error_json = err.args[0].decode(UTF_8)
         error = json.loads(error_json)[ERROR]
         if error == UNAUTHORIZED_CLIENT_ERROR:
@@ -120,7 +119,6 @@ keycloak_admin = build_client()
 
 
 def update_realm_settings(realm_name: str, payload: dict[str, Any]):
-    print("update_realm_settings...........")
     if not config["UPDATE_ALL_REALM"]:
         keycloak_admin.update_realm(realm_name, payload=payload)
         return True
@@ -129,18 +127,16 @@ def update_realm_settings(realm_name: str, payload: dict[str, Any]):
     for realm in all_realms:
         keycloak_admin.update_realm(realm[REALM], payload=payload)
 
-    return True
     
 
 
 def create_client(payload: dict[str, Any]):
-    print("create_client..........")
-    print(payload)
     internal_client_id = None
+    client_secret = None
+
     try:
         internal_client_id = keycloak_admin.create_client(payload=payload)
     except exceptions.KeycloakPostError as err:
-        print("exception raised creating login client ...................")
         error_json = err.args[0].decode(UTF_8)
         error_message = json.loads(error_json)[ERROR_MESSAGE]
         if "already exists" in error_message:
@@ -154,22 +150,22 @@ def create_client(payload: dict[str, Any]):
             raise
 
     client_secret = keycloak_admin.get_client_secrets(client_id=internal_client_id)
+    if (not client_secret.get("value")):
+        client_secret = keycloak_admin.generate_client_secrets(client_id=internal_client_id)
+
     return {
         "client_id": internal_client_id,
         "secret_key": client_secret
     }
 
 def update_client(internal_client_id: str, payload: dict[str, Any]):
-    print("updating client")
     keycloak_admin.update_client(client_id=internal_client_id, payload=payload)
     return True
 
 def delete_cliet(internal_client_id: str):
-    print("deleting client.............")
     keycloak_admin.delete_client(internal_client_id)
 
 def assign_client_role(internal_client_id: str, role_name: str):
-    print("assign_client_role.............")
     role = keycloak_admin.get_realm_role(role_name)
     service_account_admin_cli = keycloak_admin.get_client_service_account_user(
         internal_client_id
@@ -178,7 +174,6 @@ def assign_client_role(internal_client_id: str, role_name: str):
     return True
 
 def delete_client_role(internal_client_id: str, role_name: str):
-    print("deleting client role.............")
     role = keycloak_admin.get_realm_role(role_name)
     service_account_admin_cli = keycloak_admin.get_client_service_account_user(
         internal_client_id
@@ -188,12 +183,12 @@ def delete_client_role(internal_client_id: str, role_name: str):
 
 
 def get_internal_client_id(client_id: str):
-    print(client_id)
     return keycloak_admin.get_client_id(client_id)
 
 # if confidential and role is not admin then can't get internal_client_id
 
 def main():
+    print("fetching initial settings")
     initial_realm_settings = keycloak_admin.get_realm(config["REALM_NAME"])
     INTERNAL_CLIENT_ID = get_internal_client_id(config["ADMIN_CLIENT_ID"])
     initial_admin_client = keycloak_admin.get_client(INTERNAL_CLIENT_ID)
@@ -201,7 +196,7 @@ def main():
     is_realm_updated = False
     is_client_updated = False
     is_role_assigned = False
-
+    is_client_created = False
 
     if not initial_realm_settings.get("loginTheme"):
         initial_realm_settings["loginTheme"] = ""
@@ -210,34 +205,46 @@ def main():
         initial_realm_settings["emailTheme"] = ""
 
     try:
-        is_realm_updated = update_realm_settings(realm_name="master", payload=update_realm_payload)
+        print("updating realm settings")
+        update_realm_settings(realm_name="master", payload=update_realm_payload)
+        is_realm_updated = True
+        print("realm settings has been updated")
+        print("creating login client")
         new_client = create_client(payload=create_client_payload)
-        is_client_updated = update_client(internal_client_id=INTERNAL_CLIENT_ID, payload=update_client_payload)
-        is_role_assigned = assign_client_role(internal_client_id=INTERNAL_CLIENT_ID, role_name="admin")
-    except:
-        print("reverting started.............")
-        print("is_realm_updated")
-        print(is_realm_updated)
-        print("is_new_client")
-        print(new_client != None)
-        print("is_client_updated")
-        print(is_client_updated)
-        print("is_role_assigned")
-        print(is_role_assigned)
-        if is_realm_updated and new_client and is_client_updated and is_role_assigned:
-            delete_client_role(internal_client_id=INTERNAL_CLIENT_ID, role_name="admin")
-            is_role_assigned = False
+        is_client_created = True
+        print("login client has been created")
+        print("updating client admin cli")
+        update_client(internal_client_id=INTERNAL_CLIENT_ID, payload=update_client_payload)
+        is_client_updated = True
+        print("client admin cli has been updated")
+        print("assigning admin cli client admin role")
+        assign_client_role(internal_client_id=INTERNAL_CLIENT_ID, role_name=ADMIN)
+        is_role_assigned = True
+        print("client admin role has been assigned to admin cli")
+    except Exception as e:
+        print("failure: ",str(e))
 
-        if is_realm_updated and new_client and is_client_updated:
+        if is_role_assigned:
+            delete_client_role(internal_client_id=INTERNAL_CLIENT_ID, role_name=ADMIN)
+            is_role_assigned = False
+            print("reverted client role settings")
+
+        if is_client_updated:
             update_client(internal_client_id=INTERNAL_CLIENT_ID, payload=initial_admin_client)
             is_client_updated = False
+            print("reverted client to original settings")
 
-        if is_realm_updated and new_client:
+
+        if is_client_created:
             delete_cliet(new_client["client_id"])
-            new_client = None
+            is_client_created = False
+            print("deleted created client")
+
 
         if is_realm_updated:    
             update_realm_settings(realm_name=config["REALM_NAME"], payload=initial_realm_settings)
             is_realm_updated = False
+            print("reverted realm to original settings")
+
 
 main()
